1315c7db5af164f0b10c0fa303a130512a7aa8d3
Rabia Arslan <rabiaarslann.11@gmail.com>
2021-02-02 03:53:03 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CampProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            string[] meyveler = new string[] { };
+
+            Product urun1 = new Product();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+            Product urun2 = new Product();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            Product[] urunler = new Product[] { urun1, urun2 };
+
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("-------------");
+            }
+            Console.WriteLine("---------Metotlar------");
+            SepetManager sepetmanager = new SepetManager();
+            sepetmanager.Ekle(urun1);
+            sepetmanager.Ekle(urun2);
+            
+            sepetmanager.Ekle2("Armut", "Yeşil Armut", 12, 10);
+            sepetmanager.Ekle2("Elma", "Yeşil Elma", 12 ,9);
+            sepetmanager.Ekle2("Karpuz", "Diyarbakır Karpuzu", 12, 8);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ SepetManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CampProject
+{
+    class SepetManager
+    {
+        public void Ekle(Product urun)
+        {
+            Console.WriteLine("Tebrikler, Sepete eklendi : "+ urun.Adi);
+
+        }
+
+        public void Ekle2( string urunAdi, string aciklama, double fiyat, int stokAdedi)
+        {
+            Console.WriteLine("Tebrikler sepete eklendi : " + urunAdi);
+        }
+    }
+}
